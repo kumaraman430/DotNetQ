01a0e751105323ec421734baf005d3b7b847ede2
kumaraman430 <ucckumaraman430@gmail.com>
2024-09-24 12:32:03 +0530

    Udated in Arrays

--- 3.Arrays/Program.cs
+++ 3.Arrays/Program.cs
@@ -8,31 +8,31 @@ namespace _3.Arrays
 {
     public class SingleDimensional_Array
     {
-        public static void Main(string[] args)
-        {
-            //int[] arr = new int[5];
-            //arr[0] = 10;
-            //arr[2] = 20;
-            //arr[4] = 30;
-            //OR
-            //int[] arr = new int[5] { 10, 20, 30, 40, 50 };
-            //OR
-            //int[] arr = new int[] { 10, 20, 30, 40 };
-            //OR
-            int[] arr = { 10, 20, 30, 40, 50 };
+        //public static void Main(string[] args)
+        //{
+        //    //int[] arr = new int[5];
+        //    //arr[0] = 10;
+        //    //arr[2] = 20;
+        //    //arr[4] = 30;
+        //    //OR
+        //    //int[] arr = new int[5] { 10, 20, 30, 40, 50 };
+        //    //OR
+        //    //int[] arr = new int[] { 10, 20, 30, 40 };
+        //    //OR
+        //    int[] arr = { 10, 20, 30, 40, 50 };
 
-            // Traversal using for loop
-            //for (int i = 0; i < arr.Length; i++)
-            //{
-            //    Console.WriteLine(arr[i]);
-            //}
+        //    // Traversal using for loop
+        //    //for (int i = 0; i < arr.Length; i++)
+        //    //{
+        //    //    Console.WriteLine(arr[i]);
+        //    //}
 
-            // Traversal using foreach loop
-            foreach (int i in arr)
-            {
-                Console.WriteLine(i);
-            }
-            Console.ReadKey();
-        }
+        //    // Traversal using foreach loop
+        //    foreach (int i in arr)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //    Console.ReadKey();
+        //}
     }
 }
