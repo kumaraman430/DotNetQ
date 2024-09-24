03350312ae9089260dc9de791c1220605be0f464
kumaraman430 <ucckumaraman430@gmail.com>
2024-09-24 15:14:32 +0530

    Update in Arrays

--- 3.Arrays/Program.cs
+++ 3.Arrays/Program.cs
@@ -35,4 +35,71 @@ namespace _3.Arrays
         //    Console.ReadKey();
         //}
     }
+
+    public class Passing_array_to_function
+    {
+        //static void printArray(int[] arr)
+        //{
+        //    Console.WriteLine("Printing array elements");
+        //    foreach (int i in arr)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+        //public static void Main(string[] args)
+        //{
+        //    int[] arr1 = { 25, 10, 20, 15, 40, 50 };
+        //    int[] arr2 = { 12, 23, 44, 11, 54 };
+
+        //    printArray(arr1);
+        //    printArray(arr2);
+
+        //    Console.ReadKey();
+        //}
+    }
+
+    public class Passing_array_to_function_Min_Num
+    {
+        //static void PrintMin(int[] arr)
+        //{
+        //    int min = arr[0];
+        //    for (int i = 1; i < arr.Length; i++)
+        //    {
+        //        if (min > arr[i])
+        //        {
+        //            min = arr[i];
+        //        }
+        //    }
+        //    Console.WriteLine("Minimum Element is : " + min);
+        //}
+        //static void PrintMax(int[] arr)
+        //{
+        //    int max = arr[0];
+        //    for (int i = 1; i < arr.Length; i++)
+        //    {
+        //        if (max < arr[i])
+        //        {
+        //            max = arr[i];
+        //        }
+        //    }
+        //    Console.WriteLine("Maximum Element is : " + max);
+        //}
+        //public static void Main(string[] args)
+        //{
+        //    int[] arr1 = { 25, 10, 20, 15, 40, 50 };
+        //    int[] arr2 = { 12, 23, 44, 11, 54 };
+
+        //    PrintMin(arr1);
+        //    PrintMin(arr2);
+        //    PrintMax(arr1);
+        //    PrintMax(arr2);
+
+        //    Console.ReadKey();
+        //}
+    }
+
+    public class MultiDimensional_Array
+    {
+
+    }
 }
