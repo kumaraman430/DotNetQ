d06efe21550fbd80fd1a38afd28724099f66848e
kumaraman430 <ucckumaraman430@gmail.com>
2024-09-30 16:00:38 +0530

    Updated Objects&Classes and Added Properties

--- 4.Object&Class/Program.cs
+++ 4.Object&Class/Program.cs
@@ -161,4 +161,259 @@ namespace _4.Object_Class
         //    }
         //}
     }
+
+    class Destructor
+    {
+        //class Employee
+        //{
+        //    public Employee()
+        //    {
+        //        Console.WriteLine("Constructor Invoked");
+        //    }
+        //    ~Employee()
+        //    {
+        //        Console.WriteLine("Destructor Invoked");
+        //    }
+        //}
+        //class EmployeeTest
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Employee emp1 = new Employee();
+        //        Employee emp2 = new Employee();
+        //    }
+        //}
+    }
+
+    class this_Example
+    {
+        //class Employee
+        //{
+        //    int Id;
+        //    string Name;
+        //    float Salary;
+        //    public Employee(int id, string name, float salary)
+        //    {
+        //        this.Id = id;
+        //        this.Name = name;
+        //        this.Salary = salary;
+        //    }
+        //    public void Display()
+        //    {
+        //        Console.WriteLine("Id : {0}\nName : {1}\nSalary : {2}", Id, Name, Salary);
+        //    }
+        //}
+        //class EmployeeTest
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Employee emp1 = new Employee(1, "Aman", 45000);
+        //        Employee emp2 = new Employee(2, "Jack", 45000000);
+
+        //        emp1.Display();
+        //        emp2.Display();
+        //    }
+        //}
+    }
+
+    class static_Example
+    {
+        //class Account
+        //{
+        //    int accno;
+        //    string name;
+        //    public static float rateofinterest = 8.8f;
+        //    public static int count = 0;
+        //    public Account(int AccNo, string Name)
+        //    {
+        //        this.accno = AccNo;
+        //        this.name = Name;
+        //        count++;
+        //    }
+
+        //    public void Display()
+        //    {
+        //        Console.WriteLine(accno + " " + name + " " + rateofinterest);
+        //    }
+        //}
+        //class AccountTest
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Account.rateofinterest = 10.5f; //Changing value
+
+        //        Account acc1 = new Account(101, "Aman");
+        //        Account acc2 = new Account(102, "Jack");
+        //        Account acc3 = new Account(103, "Spaorrow");
+
+        //        acc1.Display();
+        //        acc2.Display();
+        //        acc3.Display();
+
+        //        Console.WriteLine("Total objects are : " + Account.count);
+        //    }
+        //}
+    }
+
+    class static_class_Example
+    {
+        //static class myMath
+        //{
+        //    public static float PI = 3.14f;
+        //    public static int cube(int n)
+        //    {
+        //        return n * n * n;
+        //    }
+        //}
+        //class myMathTest
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Console.WriteLine("Value of PI is : {0}", myMath.PI);
+        //        Console.WriteLine("Cube of 3 is : {0}",myMath.cube(3));
+        //    }
+        //}
+    }
+
+    class static_Constructor_Example
+    {
+        //class Account
+        //{
+        //    int id;
+        //    string name;
+        //    static float rateofinterest;
+        //    public Account(int Id, string Name)
+        //    {
+        //        this.id = Id;
+        //        this.name = Name;
+        //    }
+        //    static Account()
+        //    {
+        //        rateofinterest = 9.5f;
+        //    }
+        //    public void Display()
+        //    {
+        //        Console.WriteLine(id + " " + name + " " + rateofinterest);
+        //    }
+        //}
+        //class AccountTest
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Account acc1 = new Account(101, "Aman");
+        //        Account acc2 = new Account(102, "Jack");
+
+        //        acc1.Display();
+        //        acc2.Display();
+        //    }
+        //}
+    }
+
+    class struct_Example
+    {
+        //public struct Rectangle
+        //{
+        //    public int width, height;
+        //}
+        //class RectangleTest
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Rectangle r = new Rectangle();
+        //        r.width = 4;
+        //        r.height = 5;
+
+        //        Console.WriteLine("Area of Rectangle is : " + (r.width * r.height));
+        //    }
+        //}
+    }
+
+    class struct_Contructor_Example
+    {
+        //struct Rectangle
+        //{
+        //    int width, height;
+
+        //    public Rectangle(int w, int h)
+        //    {
+        //        width = w;
+        //        height = h;
+        //    }
+        //    public void areaofRectangle()
+        //    {
+        //        Console.WriteLine("Area of Rectangle is : {0}", (width * height));
+        //    }
+        //}
+        //class RectangleTest
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Rectangle rect = new Rectangle(5, 6);
+        //        rect.areaofRectangle();
+        //    }
+        //}
+    }
+
+    class Enum_Example
+    {
+        //class Simple_Enum_Example
+        //{
+        //    enum Season { WINTER, SPRING, SUMMER, FALL }
+        //    static void Main(string[] args)
+        //    {
+        //        int x = (int)Season.WINTER;
+        //        int y = (int)Season.SUMMER;
+
+        //        Console.WriteLine("WINTER = {0}", x);
+        //        Console.WriteLine("SUMMER = {0}", y);
+        //    }
+        //}
+
+        //class Change_Start_Index_Enum_Example
+        //{
+        //    enum Season { WINTER = 10, SPRING, SUMMER, FALL }
+        //    static void Main(string[] args)
+        //    {
+        //        int x = (int)Season.WINTER;
+        //        int y = (int)Season.SUMMER;
+
+        //        Console.WriteLine("WINTER = {0}", x);
+        //        Console.WriteLine("SUMMER = {0}", y);
+        //    }
+        //}
+
+        //class Days_Enum_Example
+        //{
+        //    enum Days { Sun, Mon, Tue, Wed, Thus, Fri, Sat }
+
+        //    static void Main(string[] args)
+        //    {
+        //        int x = (int)Days.Sun;
+        //        int y = (int)Days.Mon;
+        //        int z = (int)Days.Sat;
+
+        //        Console.WriteLine("Sun = {0}", x);
+        //        Console.WriteLine("Mon = {0}", y);
+        //        Console.WriteLine("Sat = {0}", z);
+        //    }
+        //}
+
+        //class traversing_Enum_Example
+        //{
+        //    enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat }
+
+        //    static void Main(string[] args)
+        //    {
+        //        foreach (string s in Enum.GetNames(typeof(Days))) // Traversing all value using getNames()
+        //        {
+        //            Console.WriteLine(s);
+        //        }
+
+        //        foreach (Days d in Enum.GetValues(typeof(Days))) // Traversing all values using getValues()
+        //        {
+        //            Console.WriteLine(d);
+        //        }
+        //    }
+        //}
+    }
 }
---
+++ 5.Properties/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.Properties
+{
+    class Properties_Example
+    {
+        //    class Employee
+        //    {
+        //        private string name;
+        //        public string Name
+        //        {
+        //            get
+        //            {
+        //                return name;
+        //            }
+        //            set
+        //            {
+        //                name = value;
+        //            }
+        //        }
+        //    }
+        //    class EmployeeTest
+        //    {
+        //        static void Main(string[] args)
+        //        {
+        //            Employee e1 = new Employee();
+        //            e1.Name = "Test";
+
+        //            Console.WriteLine("Employee Name : {0}", e1.Name);
+        //        }
+        //    }
+    }
+
+    class Setting_Logic_Properties_Example
+    {
+        //    class Employee
+        //    {
+        //        private string name;
+        //        public string Name
+        //        {
+        //            get
+        //            {
+        //                return name;
+        //            }
+        //            set
+        //            {
+        //                name = value + "\nTest2";
+        //            }
+        //        }
+        //    }
+        //    class EmployeeTest
+        //    {
+        //        static void Main(string[] args)
+        //        {
+        //            Employee e = new Employee();
+        //            e.Name = "Test1";
+
+        //            Console.WriteLine("Employee Name : \n{0}", e.Name);
+        //        }
+        //    }
+    }
+
+    class ReadOnly_Properties_Example
+    {
+        class Employee
+        {
+            private static int counter;
+            public Employee()
+            {
+                counter++;
+            }
+
+            public static int Counter
+            {
+                get
+                {
+                    return counter;
+                }
+            }
+        }
+        class EmployeeTest
+        {
+            static void Main(string[] args)
+            {
+                Employee e1 = new Employee();
+                Employee e2 = new Employee();
+                Employee e3 = new Employee();
+
+                //e1.Counter = 10; //Compile Time Error : Can't set value
+
+                Console.WriteLine("No. of Employees : " + Employee.Counter);
+            }
+        }
+    }
+
+}
