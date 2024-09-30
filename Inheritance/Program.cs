e8f4b332e5efbb877a3f421a9b85586a8a354423
kumaraman430 <ucckumaraman430@gmail.com>
2024-09-30 17:28:49 +0530

    Added inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Single_Level_Inheritance
+    {
+        class Inheriting_Fields
+        {
+            //class Employee // Base Class
+            //{
+            //    public float salary = 50000;
+            //}
+            //class Programmer : Employee //Derived Class
+            //{
+            //    public float bonus = 25000;
+            //}
+            //class Single_Level_Inheritance_Test
+            //{
+            //    static void Main(string[] args)
+            //    {
+            //        Programmer p = new Programmer();
+            //        Console.WriteLine("Salary : {0}", p.salary);
+            //        Console.WriteLine("Bonus : {0}", p.bonus);
+            //    }
+            //}
+        }
+        class Inheriting_Methods
+        {
+            //class Animal // Base Class
+            //{
+            //    public void eat()
+            //    {
+            //        Console.WriteLine("Eating...");
+            //    }
+            //}
+            //class Dog : Animal // Derived Class
+            //{
+            //    public void bark()
+            //    {
+            //        Console.WriteLine("Barking...");
+            //    }
+            //}
+
+            //class Single_Level_Inheritance_Test
+            //{
+            //    static void Main(string[] args)
+            //    {
+            //        Dog d = new Dog();
+            //        d.eat();
+            //        d.bark();
+            //    }
+            //}
+        }
+    }
+    class Multi_Level_Inheritance
+    {
+        //class Animal // Base Class
+        //{
+        //    public void eat()
+        //    {
+        //        Console.WriteLine("Eating...");
+        //    }
+        //}
+        //class Dog : Animal // Derived Class
+        //{
+        //    public void bark()
+        //    {
+        //        Console.WriteLine("Barking...");
+        //    }
+        //}
+        //class BabyDog : Dog // Derived Class
+        //{
+        //    public void weep()
+        //    {
+        //        Console.WriteLine("Weeping...");
+        //    }
+        //}
+
+        //class InheritanceTest
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        BabyDog bd = new BabyDog();
+        //        bd.eat();
+        //        bd.bark();
+        //        bd.weep();
+        //    }
+        //}
+    }
+}
